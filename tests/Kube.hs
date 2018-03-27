jml/compare-revisions

commit 62d330a439f2bae629a18ced068b2a575b1a1c63

Make images a property of KubeObject
Rather than holding on to the JSON AST and then later mining it for possible
images, we get all the images when we parse the KubeObject.

cmd/compare-images/Main.hs
@@ -25,7 +25,7 @@ formatDiff = Map.foldMapWithKey formatSingleDiff
     formatSingleDiff kubeObj diffs =
       mconcat ([ formatKubeObject kubeObj, "\n" ] <>
                [ "  " <> formatImageDiff diff <> "\n" | diff <- diffs ]) <> "\n"
-    formatKubeObject (KubeObject namespace kind name) = namespace <> "/" <> name <> " (" <> kind <> ")"
+    formatKubeObject (KubeObject namespace kind name _) = namespace <> "/" <> name <> " (" <> kind <> ")"
 
     formatImage name label = name <> ":" <> fromMaybe "default" label
 

src/CompareRevisions/Kube.hs
@@ -7,7 +7,6 @@ module CompareRevisions.Kube
     -- * Environments
   , Env
   , loadEnvFromDisk
-  , loadEnvFromCluster
   , ProcessError(..)
     -- * Images
   , Image(..)
@@ -22,6 +21,7 @@ module CompareRevisions.Kube
 
 import Protolude hiding (diff)
 
+import Control.Monad (fail)
 import qualified Data.Aeson as Aeson
 import Data.Aeson (Value(..), (.:), (.:?))
 import qualified Data.ByteString as ByteString
@@ -39,17 +39,29 @@ data KubeObject
   = KubeObject { namespace :: Namespace
                , kind :: Kind
                , name :: Name
+                 -- | All the images mentioned in the object definition.
+               , images :: [Image]
                } deriving (Eq, Ord, Show, Generic)
 
 instance Aeson.ToJSON KubeObject
 
 instance Aeson.FromJSON KubeObject where
-  parseJSON = Aeson.withObject "KubeObject" $ \v -> do
-    kind <- v .: "kind"
-    metadata <- v .: "metadata"
+  parseJSON v = Aeson.withObject "KubeObject" (\obj -> do
+    kind <- obj .: "kind"
+    metadata <- obj .: "metadata"
     name <- metadata .: "name"
     namespace <- metadata .:? "namespace"
-    pure $ KubeObject (fromMaybe "default" namespace) kind name
+    images <- traverse Aeson.parseJSON (findAll "image" v)
+    pure $ KubeObject (fromMaybe "default" namespace) kind name images) v
+
+
+-- | Find all instances of a given key in a JSON value.
+findAll :: Text -> Value -> [Value]
+findAll key value =
+  case value of
+    Object obj -> maybeToList (HashMap.lookup key obj) <> concatMap (findAll key) obj
+    Array arr -> concatMap (findAll key) arr
+    _ -> []
 
 -- | A Kubernetes namespace. e.g. "default".
 type Namespace = Text
@@ -98,6 +110,20 @@ data Image
           , label :: Maybe ImageLabel
           } deriving (Eq, Ord, Show)
 
+instance Aeson.ToJSON Image where
+  toJSON (Image name label) = Aeson.toJSON $
+    case label of
+      Nothing -> name
+      Just label' -> name <> ":" <> label'
+
+instance Aeson.FromJSON Image where
+  parseJSON = Aeson.withText "Image" $ \img ->
+    case splitOn ":" img of
+      [name] -> pure (Image name Nothing)
+      [name, label] -> pure (Image name (Just label))
+      _ -> fail "Too many colons in image name"
+
+
 type ImageName = Text
 type ImageLabel = Text
 
@@ -108,30 +134,8 @@ type ImageSet = Map ImageName (Maybe ImageLabel)
 --
 -- Because a single definition can have multiple images, we return a map of
 -- image name to image label, where the label is optional.
-getImageSet :: Value -> ImageSet
-getImageSet value =
-  Map.fromList [ (name, label) | Image name label <- images ]
-  where
-    images = mapMaybe parseImageName (getImageNames value)
-
-    -- | Get all the names of images within a JSON value.
-    getImageNames (Object obj) =
-      image <> rest
-      where
-        image =
-          case HashMap.lookup "image" obj of
-            Just (String name) -> [name]
-            _ -> []
-        rest = concatMap getImageNames obj
-    getImageNames (Array arr) = concatMap getImageNames arr
-    getImageNames _ = []
-
-    -- | Parse an image name.
-    parseImageName imageName =
-      case splitOn ":" imageName of
-        [name] -> Just (Image name Nothing)
-        [name, label] -> Just (Image name (Just label))
-        _ -> Nothing
+getImageSet :: KubeObject -> ImageSet
+getImageSet kubeObj = Map.fromList [ (name, label) | Image name label <- images kubeObj ]
 
 -- | Possible difference between image sets.
 data ImageDiff
@@ -169,32 +173,15 @@ mapDiff source target = Map.fromList (go (Map.toAscList source) (Map.toAscList t
         GT -> (yKey, Removed yValue):go xs ys'
 
 
--- | Here, an environment is a mapping from Kubernetes to their definitions.
-type Env = Map KubeObject Value
+-- | Here, an environment is collection of Kubernetes objects.
+type Env = [KubeObject]
 
 loadEnvFromDisk :: MonadIO m => FilePath -> m Env
 loadEnvFromDisk directory = do
   files <- getFiles directory
   let yamlFiles = [ f | f <- files, takeExtension f == ".yaml" ]
   bytes <- traverse (liftIO . ByteString.readFile) yamlFiles
-  let values = mapMaybe Yaml.decode bytes -- ignore files that don't parse to yaml
-  pure (Map.fromList (mapMaybe valueToPair values)) -- ignore yaml that doesn't look like kubeobject
-  where
-    valueToPair v =
-      case Aeson.fromJSON v of
-        Aeson.Error _ -> Nothing
-        Aeson.Success kubeObj -> Just (kubeObj, v)
-
--- | Given a @kubeConfig@ and some @kubeObjects@, fetch their definitions from
--- a cluster.
---
--- If anything goes wrong running the @kubectl@ process at any time, just give
--- up.
-loadEnvFromCluster :: Maybe FilePath -> [KubeObject] -> ExceptT ProcessError IO Env
-loadEnvFromCluster kubeConfig kubeObjects = do
-  bytes <- traverse (getClusterDefinition kubeConfig) kubeObjects
-  let objToBytes = zip kubeObjects bytes
-  pure (Map.fromList (mapMaybe (traverse (Aeson.decode . toS)) objToBytes))
+  pure $ mapMaybe Yaml.decode bytes -- ignore files that don't parse to yaml
 
 -- | Given two sets of Kubernetes objects, return the images that differ
 -- between them.
@@ -202,8 +189,8 @@ getDifferingImages :: Env -> Env -> Map KubeObject [ImageDiff]
 getDifferingImages sourceEnv targetEnv =
   Map.mapMaybe getImageDiffs (mapDiff sourceImages targetImages)
   where
-    sourceImages = map getImageSet sourceEnv
-    targetImages = map getImageSet targetEnv
+    sourceImages = Map.fromList [(k, getImageSet k) | k <- sourceEnv]
+    targetImages = Map.fromList [(k, getImageSet k) | k <- targetEnv]
 
     -- If a Kubernetes object was added, we don't really care about the images
     -- within.

commit c51e8592666047bdc80ff03ed7e520822cc5b256

Distinguish between KubeID and KubeObject
Fixes a bug introduced in 62d330a where we would get absolutely no diffs.
Adds tests that would have caught the bug.

cmd/compare-images/Main.hs
@@ -5,7 +5,7 @@ import Protolude hiding (diff)
 import qualified Data.Map as Map
 import Options.Applicative
 import CompareRevisions.Kube
-  ( KubeObject(..)
+  ( KubeID(..)
   , ImageDiff(..)
   , loadEnvFromDisk
   , getDifferingImages
@@ -19,13 +19,13 @@ options = Config
   <$> argument str (metavar "SOURCE" <> help "The environment with the newer images.")
   <*> argument str (metavar "DESTINATION" <> help "The environment with the older images.")
 
-formatDiff :: Map KubeObject [ImageDiff] -> Text
+formatDiff :: Map KubeID [ImageDiff] -> Text
 formatDiff = Map.foldMapWithKey formatSingleDiff
   where
     formatSingleDiff kubeObj diffs =
       mconcat ([ formatKubeObject kubeObj, "\n" ] <>
                [ "  " <> formatImageDiff diff <> "\n" | diff <- diffs ]) <> "\n"
-    formatKubeObject (KubeObject namespace kind name _) = namespace <> "/" <> name <> " (" <> kind <> ")"
+    formatKubeObject (KubeID namespace kind name) = namespace <> "/" <> name <> " (" <> kind <> ")"
 
     formatImage name label = name <> ":" <> fromMaybe "default" label
 

src/CompareRevisions/API.hs
@@ -88,16 +88,16 @@ instance L.ToHtml RootPage where
           Just path' -> toS (uriToString identity (path' `relativeTo` externalURL) "")
 
 
-newtype ImageDiffs = ImageDiffs (Maybe (Map Kube.KubeObject [Kube.ImageDiff])) deriving (Eq, Ord, Show, Generic)
+newtype ImageDiffs = ImageDiffs (Maybe (Map Kube.KubeID [Kube.ImageDiff])) deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON ImageDiffs where
   -- I *think* we can't get a default instance because Aeson cowardly refuses
   -- to objects where the keys are objects.
   toJSON (ImageDiffs diffs) = toJSON (Map.fromList . map reshapeKeys . Map.toList <$> diffs)
     where
-      reshapeKeys (kubeObj, diff) =
-        ( Kube.namespacedName kubeObj
-        , Map.fromList [ ("kind" :: Text, toJSON (Kube.kind kubeObj))
+      reshapeKeys (kubeID, diff) =
+        ( Kube.namespacedName kubeID
+        , Map.fromList [ ("kind" :: Text, toJSON (Kube.kind kubeID))
                        , ("diff", toJSON diff)
                        ]
         )

src/CompareRevisions/Engine.hs
@@ -59,7 +59,7 @@ data Error
 data ClusterDiff
  = ClusterDiff
  { revisionDiffs :: Map Kube.ImageName (Either Error [Git.Revision])
- , imageDiffs :: Map Kube.KubeObject [Kube.ImageDiff]
+ , imageDiffs :: Map Kube.KubeID [Kube.ImageDiff]
  }
  deriving (Show)
 
@@ -208,7 +208,7 @@ syncRepo repoRoot url = do
   where
     repoPath = Config.getRepoPath repoRoot url
 
-compareImages :: MonadIO io => FilePath -> Config.ValidConfig -> ExceptT Error io (Map Kube.KubeObject [Kube.ImageDiff])
+compareImages :: MonadIO io => FilePath -> Config.ValidConfig -> ExceptT Error io (Map Kube.KubeID [Kube.ImageDiff])
 compareImages gitRepoDir Config.ValidConfig{..} = withExceptT GitError $ do
   let Config.ConfigRepo{..} = configRepo
   repoPath <- syncRepo gitRepoDir url

src/CompareRevisions/Kube.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 module CompareRevisions.Kube
   ( -- * Objects in Kubernetes
-    KubeObject(..)
+    KubeID(..)
+  , KubeObject(..)
   , namespacedName
   , getImageSet
     -- * Environments
@@ -35,34 +36,26 @@ import System.FilePath ((</>), takeExtension)
 import System.Posix.Files (getFileStatus, isDirectory)
 import System.Process (readProcessWithExitCode)
 
--- | A Kubernetes object
-data KubeObject
-  = KubeObject { namespace :: Namespace
-               , kind :: Kind
-               , name :: Name
-                 -- | All the images mentioned in the object definition.
-               , images :: [Image]
-               } deriving (Eq, Ord, Show, Generic)
+-- | Identifies a Kubernetes object within a cluster.
+data KubeID
+  = KubeID
+  { namespace :: Namespace
+  , kind :: Kind
+  , name :: Name
+  } deriving (Eq, Ord, Show)
 
-instance Aeson.ToJSON KubeObject
-
-instance Aeson.FromJSON KubeObject where
-  parseJSON v = Aeson.withObject "KubeObject" (\obj -> do
+instance Aeson.FromJSON KubeID where
+  parseJSON = Aeson.withObject "KubeID" (\obj -> do
     kind <- obj .: "kind"
     metadata <- obj .: "metadata"
     name <- metadata .: "name"
     namespace <- metadata .:? "namespace"
-    images <- traverse Aeson.parseJSON (findAll "image" v)
-    pure $ KubeObject (fromMaybe "default" namespace) kind name images) v
+    pure $ KubeID (fromMaybe "default" namespace) kind name)
 
+-- | The fully qualified name of a Kubernetes object. e.g. "default/authfe".
+namespacedName :: KubeID -> Text
+namespacedName KubeID{..} = namespace <> "/" <> name
 
--- | Find all instances of a given key in a JSON value.
-findAll :: Text -> Value -> [Value]
-findAll key value =
-  case value of
-    Object obj -> maybeToList (HashMap.lookup key obj) <> concatMap (findAll key) obj
-    Array arr -> concatMap (findAll key) arr
-    _ -> []
 
 -- | A Kubernetes namespace. e.g. "default".
 type Namespace = Text
@@ -73,9 +66,28 @@ type Kind = Text
 -- | The name of a thing in Kubernetes. e.g. "authfe".
 type Name = Text
 
--- | The fully qualified name of a Kubernetes object. e.g. "default/authfe".
-namespacedName :: KubeObject -> Text
-namespacedName KubeObject{..} = namespace <> "/" <> name
+
+-- | A Kubernetes object
+data KubeObject
+  = KubeObject
+  { -- | The identifier for this object.
+    id :: KubeID
+    -- | All the images mentioned in the object definition.
+  , images :: [Image]
+  } deriving (Eq, Ord, Show)
+
+instance Aeson.FromJSON KubeObject where
+  parseJSON v = KubeObject <$> Aeson.parseJSON v <*> traverse Aeson.parseJSON (findAll "image" v)
+
+
+-- | Find all instances of a given key in a JSON value.
+findAll :: Text -> Value -> [Value]
+findAll key value =
+  case value of
+    Object obj -> maybeToList (HashMap.lookup key obj) <> concatMap (findAll key) obj
+    Array arr -> concatMap (findAll key) arr
+    _ -> []
+
 
 
 data ProcessError = ProcessError ExitCode ByteString deriving (Eq, Show)
@@ -83,8 +95,8 @@ data ProcessError = ProcessError ExitCode ByteString deriving (Eq, Show)
 -- | Load the definition of a Kubernetes object from a cluster.
 --
 -- Uses @kubectl@ under the hood.
-getClusterDefinition :: Maybe FilePath -> KubeObject -> ExceptT ProcessError IO ByteString
-getClusterDefinition kubeConfig KubeObject{..} = do
+getClusterDefinition :: Maybe FilePath -> KubeID -> ExceptT ProcessError IO ByteString
+getClusterDefinition kubeConfig KubeID{..} = do
   (exitCode, out, err) <- lift $ readProcessWithExitCode "kubectl" kubectlArgs ""
   case exitCode of
     ExitSuccess -> pure (toS out)
@@ -186,12 +198,12 @@ loadEnvFromDisk directory = do
 
 -- | Given two sets of Kubernetes objects, return the images that differ
 -- between them.
-getDifferingImages :: Env -> Env -> Map KubeObject [ImageDiff]
+getDifferingImages :: Env -> Env -> Map KubeID [ImageDiff]
 getDifferingImages sourceEnv targetEnv =
   Map.mapMaybe getImageDiffs (mapDiff sourceImages targetImages)
   where
-    sourceImages = Map.fromList [(k, getImageSet k) | k <- sourceEnv]
-    targetImages = Map.fromList [(k, getImageSet k) | k <- targetEnv]
+    sourceImages = Map.fromList [(id k, getImageSet k) | k <- sourceEnv]
+    targetImages = Map.fromList [(id k, getImageSet k) | k <- targetEnv]
 
     -- If a Kubernetes object was added, we don't really care about the images
     -- within.

tests/Kube.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 module Kube (tests) where
 
 import Protolude
@@ -8,6 +9,7 @@ import Data.String (String)
 import qualified Data.Yaml as Yaml
 import qualified Data.Text as Text
 import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck ((===), (==>))
 import qualified Test.QuickCheck as QuickCheck
 import Test.Tasty (TestTree)
 import Test.Tasty.Hspec (testSpec, describe, it, shouldBe)
@@ -16,14 +18,20 @@ import qualified CompareRevisions.Kube as Kube
 
 
 kubeObjects :: QuickCheck.Gen Kube.KubeObject
-kubeObjects =
-  Kube.KubeObject <$> text <*> text <*> text <*> QuickCheck.listOf images'
+kubeObjects = Kube.KubeObject <$> kubeIDs <*> QuickCheck.listOf images'
+
+kubeIDs :: QuickCheck.Gen Kube.KubeID
+kubeIDs =
+  Kube.KubeID <$> text <*> text <*> text
 
 images' :: QuickCheck.Gen Kube.Image
 images' =
-  Kube.Image <$> text <*> QuickCheck.oneof [ pure Nothing
-                                           , Just <$> text
-                                           ]
+  Kube.Image <$> text <*> labels
+
+labels :: QuickCheck.Gen (Maybe Kube.ImageLabel)
+labels = QuickCheck.oneof [ pure Nothing
+                          , Just <$> text
+                          ]
 
 text :: QuickCheck.Gen Text
 text = toS <$> (QuickCheck.arbitrary :: QuickCheck.Gen String)
@@ -35,14 +43,16 @@ tests = testSpec "Kube" $ do
     it "parses normal YAML files" $ do
       let parsed = Yaml.decodeEither (toS example)
       parsed `shouldBe` Right Kube.KubeObject
-        { namespace = "cortex"
-        , name = "ruler"
-        , kind = "Deployment"
+        { id = Kube.KubeID { namespace = "cortex"
+                           , name = "ruler"
+                           , kind = "Deployment"
+                           }
         , images = [ Kube.Image { name = "quay.io/weaveworks/cortex-ruler"
                                 , label = Just "master-f7f6cf9e"
                                 }
                    ]
         }
+
   describe "Image Sets" $ do
     prop "Image set has all image names" $
       QuickCheck.forAll kubeObjects $ \obj ->
@@ -50,6 +60,30 @@ tests = testSpec "Kube" $ do
     it "Finds all the images in our example" $ do
       let Right obj = Yaml.decodeEither (toS example)
       Kube.getImageSet obj `shouldBe` Map.fromList [("quay.io/weaveworks/cortex-ruler", Just "master-f7f6cf9e")]
+
+  describe "getDifferingImages" $ do
+    prop "shows nothing for identical environments" $
+      QuickCheck.forAll (QuickCheck.listOf kubeObjects) $ \env ->
+      Kube.getDifferingImages env env === Map.empty
+
+    prop "detects new images" $
+      QuickCheck.forAll kubeIDs $ \kubeID -> QuickCheck.forAll images' $ \image ->
+      let obj1 = Kube.KubeObject { id = kubeID, images = [image] }
+          obj2 = Kube.KubeObject { id = kubeID, images = [] }
+      in Kube.getDifferingImages [obj1] [obj2] === Map.singleton kubeID [Kube.ImageAdded (Kube.name (image :: Kube.Image)) (Kube.label image)]
+
+    prop "detects removed images" $
+      QuickCheck.forAll kubeIDs $ \kubeID -> QuickCheck.forAll images' $ \image ->
+      let obj1 = Kube.KubeObject { id = kubeID, images = [] }
+          obj2 = Kube.KubeObject { id = kubeID, images = [image] }
+      in Kube.getDifferingImages [obj1] [obj2] === Map.singleton kubeID [Kube.ImageRemoved (Kube.name (image :: Kube.Image)) (Kube.label image)]
+
+    prop "detects changed images" $
+      QuickCheck.forAll kubeIDs $ \kubeID -> QuickCheck.forAll images' $ \image -> QuickCheck.forAll labels $ \newLabel ->
+      newLabel /= Kube.label image ==>
+      let obj1 = Kube.KubeObject { id = kubeID, images = [image] }
+          obj2 = Kube.KubeObject { id = kubeID, images = [image { Kube.label = newLabel }] }
+      in Kube.getDifferingImages [obj1] [obj2] === Map.singleton kubeID [Kube.ImageChanged (Kube.name (image :: Kube.Image)) (Kube.label image) newLabel]
 
 
 example :: Text